46aef4ca7bc75d15e8a0a305ca41de01b993fdf9
Kira <kirubelteshome6202@gmail.com>
2024-10-18 10:59:29 +0300

    Task-2

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_database():
+    try:    
+        db = mysql.connector.connect(
+
+            host ='localhost'
+            user = 'root'
+            password ='root'
+        )
+        if db.is_connected():
+            mycursor = db.cursor()
+            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+    except Error as e:
+        print(f"Error: {e}")
+    
+    finally:
+
+        if db.is_connected():
+            mycursor.close()
+            db.close()
+            print("MySQL connection is closed.")
+
+    if __name__ == "__main__":
+        create_database
