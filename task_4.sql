da4b55e95392afeb1696170488f70f6c42e1e0a7
Kira <kirubelteshome6202@gmail.com>
2024-10-20 10:52:26 +0300

    task-5

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    COLUMN_KEY, 
+    EXTRA 
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
