becd756fc6ccabfbf6be0cd7741e77d7e7927b6f
Kira <kirubelteshome6202@gmail.com>
2024-10-20 18:45:08 +0300

    updated

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,28 +1,29 @@
 import mysql.connector
+from mysql.connector import errorcode
 
+try:
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="calistus",
+    )
 
+    my_cursor = mydb.cursor()
+    try:
+        my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+    except mysql.connector.Error as err:
+        print(f"Failed to create database: {err}")
 
-def create_database():
-    try:    
-        db = mysql.connector.connect(
+    my_cursor.close()
+    mydb.close()
 
-            host ='localhost'
-            user = 'root'
-            password ='root'
-        )
-        if db.is_connected():
-            mycursor = db.cursor()
-            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            print("Database 'alx_book_store' created successfully!")
-    except mysql.connector.Error as e:
-        print(f"Error: {e}")
-    
-    finally:
-
-        if db.is_connected():
-            mycursor.close()
-            db.close()
-            print("MySQL connection is closed.")
-
-    if __name__ == "__main__":
-        create_database
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The username or password is incorrect.")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist.")
+    else:
+        print(err)
+else:
+    mydb.close()
