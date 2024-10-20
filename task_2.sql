936366ba35a5894ff8f4d58ac517a98ccf7b33f7
Kira <kirubelteshome6202@gmail.com>
2024-10-20 10:41:05 +0300

    Task-3

--- task_2.sql
+++ task_2.sql
@@ -1,3 +1,10 @@
+USE alx_book_store;
+
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
 
 
 CREATE TABLE IF NOT EXISTS Books (
@@ -7,13 +14,6 @@ CREATE TABLE IF NOT EXISTS Books (
     price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-    
-);
-
-
-CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
 );
 
 
@@ -28,17 +28,16 @@ CREATE TABLE IF NOT EXISTS Customers (
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS Order_details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
-       
 );
