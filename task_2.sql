7464b10cda05144932097e0baf902fda6bfddfdd
Kira <kirubelteshome6202@gmail.com>
2024-10-20 10:15:07 +0300

    Task-3

--- task_2.sql
+++ task_2.sql
@@ -1,10 +1,5 @@
--- Create the Authors table
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
 
--- Create the Books table
+
 CREATE TABLE IF NOT EXISTS books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -12,11 +7,16 @@ CREATE TABLE IF NOT EXISTS books (
     price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
+    
 );
 
--- Create the Customers table
+
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+
 CREATE TABLE IF NOT EXISTS customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -24,26 +24,21 @@ CREATE TABLE IF NOT EXISTS customers (
     address TEXT
 );
 
--- Create the Orders table
+
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
 );
 
--- Create the Order Details table
+
 CREATE TABLE IF NOT EXISTS order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(order_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(book_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+       
 );
